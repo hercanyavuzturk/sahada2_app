162d6320c026076a3fa5e3d5048effc5fe199e11
Hercan Yavuztürk <hercanyavuzturk@gmail.com>
2022-11-24 15:27:40 +0300

    Initial commit

---
+++ lib/Services/global_variables.dart
@@ -0,0 +1,2 @@
+String loginUrlImage = "https://wallpaper.dog/large/17048551.jpg";
+String signUpUrlImage = "https://st.depositphotos.com/2464895/2808/i/450/depositphotos_28080461-stock-photo-green-soccer-field-bright-spotlights.jpg";
